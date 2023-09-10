9666a456ddc6cd08c33cfe1bfe868975fa4c7350
Sadiq <113123466+sadiqalhumood@users.noreply.github.com>
2023-09-10 12:06:01 -0400

    working on JS file, created countdown function

--- popup.js
+++ popup.js
@@ -1,2 +1,19 @@
 let countdown;
 
+const timerDisplay = document.getElementById('timer');
+
+const startButton = document.getElementById('startButton');
+
+const resetButton = document.getElementById('resetButton');
+
+let timeLeft = 25 * 60;
+
+function countdown() {
+  let minutes = Math.floor(timeLeft / 60);
+  let seconds = timeLeft % 60;
+
+  if (seconds < 10) {
+    seconds = "0" + seconds;
+  }
+
+}
