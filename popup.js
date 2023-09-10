51d3fcc9dc81d9def94dbd747e0d628ececc8243
Sadiq <113123466+sadiqalhumood@users.noreply.github.com>
2023-09-10 12:35:19 -0400

    created functionality for startButton and started on stopping and resetting timer

--- popup.js
+++ popup.js
@@ -1,4 +1,4 @@
-let countdown;
+let countdownInterval;
 
 const timerDisplay = document.getElementById('timer');
 
@@ -16,4 +16,20 @@ function countdown() {
     seconds = "0" + seconds;
   }
 
+  timerDisplay.textContent = `${minutes}:${seconds}`;
+  timeLeft--
+
+  if (timeLeft < 0) {
+    clearInterval(countdownInterval);
+  }
 }
+
+function startTimer() {
+  clearInterval(countdownInterval);
+
+  timeLeft = 25 * 60;
+
+  countdownInterval = setInterval(countdown, 1000)
+}
+
+startButton.addEventListener("click", startTimer);
